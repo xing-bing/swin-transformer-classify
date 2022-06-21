137b4a1621acdbd535b99e7aa7c421428e6a524e
xing-bing <47670651+xing-bing@users.noreply.github.com>
2022-06-21 23:47:22 +0800

    Add files via upload

---
+++ swin_transformer/predict.py
@@ -0,0 +1,69 @@
+import os
+import argparse
+import json
+
+import torch
+from PIL import Image
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+from model import swin_tiny_patch4_window7_224 as create_model
+
+def main(args):
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+    img_size = 224
+    data_transform = transforms.Compose(
+        [transforms.Resize(int(img_size * 1.14)),
+         transforms.CenterCrop(img_size),
+         transforms.ToTensor(),
+         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
+    # load image
+    img_path = args.img_path
+    assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
+    img = Image.open(img_path)
+    plt.imshow(img)
+    # [N, C, H, W]
+    img = data_transform(img)
+    # expand batch dimension
+    img = torch.unsqueeze(img, dim=0)
+
+    # read class_indict
+    json_path = './class_indices.json'
+    assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
+
+    json_file = open(json_path, "r")
+    class_indict = json.load(json_file)
+
+    # create model
+    model = create_model(num_classes=args.num_classes).to(device)
+    # load model weights
+    assert os.path.exists(args.weights), "weights file: '{}' not exist.".format(args.weights)
+    model_weight_path = args.weights
+    model.load_state_dict(torch.load(model_weight_path, map_location=device))
+    model.eval()
+    with torch.no_grad():
+        # predict class
+        output = torch.squeeze(model(img.to(device))).cpu()
+        predict = torch.softmax(output, dim=0)
+        predict_cla = torch.argmax(predict).numpy()
+
+    print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_cla)],
+                                                 predict[predict_cla].numpy())
+    plt.title(print_res)
+    for i in range(len(predict)):
+        print("class: {:10}   prob: {:.3}".format(class_indict[str(i)],
+                                                  predict[i].numpy()))
+    plt.show()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_classes', type=int, default=17)
+    parser.add_argument('--img-path', type=str, default="./bluebell.jpg")
+    parser.add_argument('--weights', type=str, default='./weights/model-9.pth',
+                        help='available weights path')
+    parser.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
+    opt = parser.parse_args()
+
+    main(opt)
